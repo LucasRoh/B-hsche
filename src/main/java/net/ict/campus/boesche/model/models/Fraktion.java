3b16784c7d5d7cfc17fd90687fa4fadd5098b05b
Lucas Rohrbach <lucas.rohrbach@ict-campus.net>
2024-02-29 12:37:25 +0100

    Gemergd

--- src/main/java/net/ict/campus/boesche/model/models/Fraktion.java
+++ src/main/java/net/ict/campus/boesche/model/models/Fraktion.java
@@ -1,14 +1,9 @@
 package net.ict.campus.boesche.model.models;
 
-import org.hibernate.annotations.Table;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-@Table ( name = "Fraktion")
+@Table(name="Fraktion")
 public class Fraktion {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
