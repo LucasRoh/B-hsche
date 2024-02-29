0020e789022ee95430ef18ed93cb488105ab1a9f
Moritz Franze <moritz.franze@ict-campus.net>
2024-02-29 09:14:19 +0100

    Anfang der Legende

---
+++ src/main/java/net/ict/campus/boesche/BoescheApplication.java
@@ -0,0 +1,13 @@
+package net.ict.campus.boesche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoescheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoescheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ict/campus/boesche/BoescheApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ict.campus.boesche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoescheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
